514406ae60303cec91cab605a719cc9d69675a48
邱彦兮 <qiuyanxi1992@gmail.com>
2021-09-06 17:21:58 +0800

    添加预算默认值和性别默认值

--- app/model/user.ts
+++ app/model/user.ts
@@ -10,8 +10,8 @@ export default app => {
     password: { type: STRING(50), comment: '密码' },
     userName: { type: STRING(50), comment: '用户名' },
     avatar: { type: STRING(100), comment: '头像地址' },
-    gender: { type: BOOLEAN, comment: '性别 0女1男' },
-    budget: { type: STRING(50), comment: '每月预算' },
+    gender: { type: BOOLEAN, comment: '性别 0女1男', defaultValue: true },
+    budget: { type: STRING(50), comment: '每月预算', defaultValue: '0' },
   });
 
   User.sync();
